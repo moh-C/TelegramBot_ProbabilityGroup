83c3d7925d2a626daaea5e463daf5f56cc889579
Aaron <mohammad.chegini@hotmail.com>
2020-05-11 03:09:15 +0430

    The approach didn't work

--- src/actions/submit.js
+++ src/actions/submit.js
@@ -1,13 +1,33 @@
 let unfilledMessage = require('../../config').unfilledMessage;
-let dataVerifier = require('../middlewares/dataVerifier');
-let dataLogger = require('../middlewares/dataLogger');
-let starter = require('../middlewares/starter');
+let starter = require('../middlewares/starter').starter;
+let groupID = '-458579843';
+let successMessage = require('../../config').successMessage;
 
 let sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 module.exports = bot => {
+    let dataVerifier = (ctx) => {
+        let members = ctx.session.members;
+        
+        for(let e in members) {
+            if(members[e].default)
+                return false;
+        }
+        return true;
+    }
+
+    let dataLogger = (ctx) => {
+        let message = ctx.from;
+    
+        bot.telegram.sendMessage(groupID, message);
+        bot.telegram.forwardMessage(groupID, ctx.chat.id, ctx.session.last_msg);
+        bot.telegram.sendMessage(groupID, ctx.session.members);
+        
+        ctx.reply(successMessage);
+    }
+    
     bot.action('submit', ctx => {
         ctx.answerCbQuery();
         
